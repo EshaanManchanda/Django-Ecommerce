3f1369c014c01c2816ba70925dd3a2e8b0b5c507
Eshaan <eshaanmanchanda01@gmail.com>
2022-12-02 18:30:41 +0530

    updated

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -79,4 +79,15 @@ LOGIN_REDIRECT_URL = '/'
 
 # CRISPY FORMS
 
+
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+0
+
+
+# EMAIL_BACKEND - 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST - 'smtp.gmail.com'
+# EMAIL_PORT - 587
+# EMAIL_USE_TLS - True
+# EMAIL_HOST_USER - "you@gmail.com"
+# EMAIL_HOST_PASSWORD - "get it from gmail app password bt setting 2 step authentication"
